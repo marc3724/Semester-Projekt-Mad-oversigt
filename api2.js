3eef941b43fafc90a2356b7ede2740b6405ca1eb
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2022-12-05 14:18:50 +0100

    json fra app til kategori

---
+++ api2.js
@@ -0,0 +1,24 @@
+// Gratis test api med array
+
+let dataAPIen ;
+
+
+let fakeURL = 'http://fakestoreapi.com/products';
+
+ //map funktion som virker på fakeApi
+ fetch(fakeURL)
+ .then((data)=>{
+ return data.json();
+})
+.then((complete)=>{
+ console.log(complete);
+ let data1 = "";
+ complete.map((values)=>{
+   data1 += `<p>${values.title}</p>`
+ });
+ dataAPIen=data1;
+}) 
+.catch((err)=>{
+ console.log(err);
+})
+
