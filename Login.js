5b6cdb1c694efaed27fb4833f633dbea38931915
TimØ <89967395+TrueVikingz@users.noreply.github.com>
2022-12-12 10:12:03 +0100

    Login og lav bruger
    Lavet en fungerende login og create bruger Html side der kan forbindes til hinanden. Der er ikke lavet forbindelse til at BD på nuværende tidspunkt.

--- Login.js
+++ Login.js
@@ -17,14 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
+        loginForm.classList.add(" form--hidden");
+        createAccountForm.classList.remove(" form--hidden");
     });
     
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
+        loginForm.classList.remove(" form--hidden");
+        createAccountForm.classList.add(" form--hidden");
     });
 
     loginForm.addEventListener("submit", e => {
